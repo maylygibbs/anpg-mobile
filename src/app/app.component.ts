8799cff87bef052c5845b6dd6885a54613d8035c
Andre Mesquita <avmesquita@gmail.com>
2021-01-12 02:15:06 +0000

    small changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,7 +33,7 @@ export class AppComponent implements OnInit {
         this.statusBar.styleDefault();
         this.splashScreen.hide();
       }, 10000);
-    });
+    }); 
   }
 
   ngOnInit(): void {
