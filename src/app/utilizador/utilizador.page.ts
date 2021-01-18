62f9395d26d86abf5bf1f7c86befc7141592d442
Andre Mesquita <avmesquita@gmail.com>
2021-01-18 13:06:55 +0000

    changes

--- src/app/utilizador/utilizador.page.ts
+++ src/app/utilizador/utilizador.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../shared/models/user';
+import { AuthenticationService } from '../shared/services/authentication.service';
 
 @Component({
   selector: 'app-utilizador',
@@ -6,7 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['utilizador.page.scss']
 })
 export class UtilizadorPage {
+  currentUser: User = null;
 
-  constructor() {}
+  constructor(
+    private router: Router
+    /*,private authenticationService: AuthenticationService*/
+  ) {
+    //this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
 
+  logout() {
+    /*this.authenticationService.logout();*/
+    this.router.navigate(['/tabs/home']);
+  }
 }
